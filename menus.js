4a835a37760a9884bc6454d60da4f4649b581ef1
Khaint002 <khainhokkazuo@gmail.com>
2025-04-11 08:44:16 +0700

    update

--- menus.js
+++
@@ -1,6 +0,0 @@
-$(".iconApp").click(function() {
-    let clickedElement = $(this); // Lấy phần tử được click
-    let buttonId = clickedElement.attr("id"); // Lấy id của nó
-
-    alert("Bạn đã click vào: " + buttonId);
-});
